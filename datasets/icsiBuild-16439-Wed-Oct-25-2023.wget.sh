aa616a11ffb6b8961065088203f89f05c53c5ace
Aritra Chakraborty <chakrabortyaritra.2002@gmail.com>
2023-11-03 02:31:16 +0530

    2nd commit

---
+++ datasets/icsiBuild-16439-Wed-Oct-25-2023.wget.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+wget    -P Signals/Bdb001 https://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/Bdb001.interaction.wav
+wget    -P Signals/Bed010 https://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/Bed010.interaction.wav
+wget    -P Signals/Bmr009 https://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/Bmr009.interaction.wav
+wget    -P Signals/Bns001 https://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/Bns001.interaction.wav
+wget    -P Signals/Bro003 https://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/Bro003.interaction.wav
+wget    -P Signals/Bsr001 https://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/Bsr001.interaction.wav
+wget    -P Signals/Btr002 https://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/Btr002.interaction.wav
+wget    -P Signals/Buw001 https://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/Buw001.interaction.wav
+wget https://groups.inf.ed.ac.uk/ami//download/temp/icsiBuild-16439-Wed-Oct-25-2023.manifest.txt
+wget https://groups.inf.ed.ac.uk/ami//download/temp/../CCBY4.0.txt
